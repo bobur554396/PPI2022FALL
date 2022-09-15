45990f7ae18df67face11587fd2885220b9a2015
bobur554396 <bobur.muhsimbaev@gmail.com>
2022-09-15 13:33:51 +0600

    Practice 4

--- w2/P/p4/5.cpp
+++ w2/P/p4/5.cpp
@@ -1,18 +1,3 @@
-/*
-
-reverse: abcd -> dcba 
-invert: ~(0010) -> 1101
-
-N -> binary -> reverse -> decimal
-
-1111
-
-0 -> 0000 -> 0000 -> 0
-2 -> 0010 -> 0100 -> 4
-3 -> 0011 -> 1100 -> 12
-12 -> 1100 -> 0011 -> 3
-*/
-
 #include <iostream>
 
 using namespace std;
